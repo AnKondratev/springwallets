84e2e68de0dd1768ec285d50c7cc0e6ee949aae0
AnKondratev <kondratev.an@inbox.ru>
2024-09-24 11:04:25 +0400

    +

--- src/main/java/an/kondratev/springwallets/service/WalletOperationService.java
+++ src/main/java/an/kondratev/springwallets/service/WalletOperationService.java
@@ -6,7 +6,6 @@ import an.kondratev.springwallets.repository.WalletRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -15,7 +14,7 @@ public class WalletOperationService {
 
     private final WalletRepository repository;
 
-    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     public void operation(WalletOperation operation) {
         Wallet wallet = repository.findByWalletId(operation.getWalletId());
         if (wallet == null) {
